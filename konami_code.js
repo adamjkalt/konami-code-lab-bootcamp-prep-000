217dc220843cdbc9c95f9aaf44014f31b4f92827
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-17 03:29:34 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // your code here
+
+let cursor = 0;
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+document.addEventListener('keydown', (e) => {
+  cursor = (e.keyCode == code[cursor]) ? cursor + 1 : 0;
+  if (cursor == code.length) activate();
+});
 }
