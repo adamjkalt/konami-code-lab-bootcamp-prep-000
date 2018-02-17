399ecc51d34b681232648d4d504fc189d34a6acd
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-17 03:31:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,3 +12,16 @@ document.addEventListener('keydown', (e) => {
 });
 }
 }
+
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
