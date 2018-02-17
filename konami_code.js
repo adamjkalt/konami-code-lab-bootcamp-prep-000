cd09ac1cd5996e62f080929d0ae850f254dbe256
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-17 03:35:40 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,11 +2,3 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
 
-$(document).on('keyup',
-
-    Konami.code(function() {
-        alert('Congratulations, 30 lives!');
-    })
-
-);
-}
