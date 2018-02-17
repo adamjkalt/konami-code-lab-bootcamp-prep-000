94277d435a4343c15b38ff2325e213b8b2e7f43b
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-17 03:40:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,8 +7,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let index = 0;
 
 document.addEventListener('keydown', function(e) {
-  // get the value of the key code from the key map
-  var key = allowedKeys[e.keyCode];
+  var key = code[e.keyCode];
   // get the value of the required key from the konami code
   var requiredKey = konamiCode[konamiCodePosition];
  
