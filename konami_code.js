dd0af0db506c1d49609ed1dc0dd44ef1a881190a
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-17 03:30:04 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,6 +6,6 @@ let cursor = 0;
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 document.addEventListener('keydown', (e) => {
   cursor = (e.keyCode == code[cursor]) ? cursor + 1 : 0;
-  if (cursor == code.length) activate();
+  if (cursor == code.length)  alert("Hurray!");
 });
 }
