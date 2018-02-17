146e8ca071613f9f8a5d8cae0426671c69caf34f
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-17 03:47:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,7 +7,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let index = 0;
 
 document.addEventListener('keydown', function(e) {
-   var key = allowedKeys[e.keyCode];
+   var key = code[e.keyCode];
   var requiredKey = code[index];
 }
   if requiredKey {
